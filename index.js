c4706b8387193491176048e446b3d5c8a1c2abc0
gfhhfhg <hgfd@dfg.com>
2024-11-19 12:57:47 +0300

    first

---
+++ index.js
@@ -0,0 +1,9 @@
+// let nnn = prompt("ent");
+// console.log(nnn ** 0.5);
+
+let a = prompt("ent a");
+let b = prompt("ent b");
+let area =(a*b);
+console.log(area);
+let p=(a * 2 + b * 2);
+console.log(p)
